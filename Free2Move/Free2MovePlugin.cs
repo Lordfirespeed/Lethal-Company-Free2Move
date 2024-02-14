6b9514f7d55324292cdf3f75987bafef4803f5fd
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-02-14 23:26:45 +0000

    add Free2Move project

---
+++ Free2Move/Free2MovePlugin.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using BepInEx;
+using BepInEx.Logging;
+
+namespace Free2Move;
+
+public sealed class Free2MovePlugin : BaseUnityPlugin
+{
+    internal new static ManualLogSource Logger = null!;
+
+    private void Awake()
+    {
+        throw new NotImplementedException();
+    }
+}
